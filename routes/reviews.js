80a5dbc9c416d0651c6be62214b1a48211dcda6e
Shobhit Kumar <146829121+kumarshobhit-1@users.noreply.github.com>
2025-08-30 21:34:03 +0530

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,11 +5,9 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js
 const reviewController = require("../controllers/reviews.js");
 
 
-// Post Review Route
 router.post("/", validateReview, isLoggedIn, wrapAsync(reviewController.createReview));
 
 
-// Delete Review Route
 router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
