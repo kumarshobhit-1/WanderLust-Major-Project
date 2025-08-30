952a9ab75fdc1d65613bf168e6eb263684587e2a
Shobhit Kumar <146829121+kumarshobhit-1@users.noreply.github.com>
2025-08-30 21:29:59 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
+const Listing = require("./listing.js"); 
+const Review = require("./review.js");  
 
 const userSchema = new Schema({
-    email : {
-        type : String,
-        required : true
-    }
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    isVerified: { type: Boolean, default: true }, 
+    createdAt: { type: Date, default: Date.now },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
 });
 
-userSchema.plugin(passportLocalMongoose); // ye hamare liye automatically hashing, passcode, salting crearte karta hai 
+userSchema.plugin(passportLocalMongoose); 
+
+userSchema.post("findOneAndDelete", async (user) => {
+    if (user) {
+        // User ke saare listings delete kar do
+        await Listing.deleteMany({ owner: user._id });
+        
+        // User ke saare reviews delete kar do (jo usne doosron ki listings par kiye the)
+        await Review.deleteMany({ author: user._id });
+    }
+});
 
 module.exports = mongoose.model('User', userSchema);
